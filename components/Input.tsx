480d46201fd4898e84a677bad5b94e820883f4bb
qazal <ghazal.nzhrzr@gmail.com>
2022-06-11 16:53:39 +0430

    feat: user interaction with terminal

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,27 +1,65 @@
 import styles from "styles/Components.module.css";
-import { useState } from "react";
+import {
+  EventHandler,
+  KeyboardEvent,
+  KeyboardEventHandler,
+  useEffect,
+  useRef,
+  useState,
+  ChangeEvent,
+} from "react";
+import { Commands } from "lib";
 
 export const Input = () => {
-  const [command, setCommand] = useState();
+  const [command, setCommand] = useState("");
+  const [isCommandValid, setIsCommandValid] = useState(true);
+  const [message, setMessage] = useState("");
+  function handleKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      Commands.forEach((c) => {
+        if (c.command === command) {
+          setIsCommandValid(true);
+          setMessage(c.message);
+        } else {
+          setIsCommandValid(false);
+          setMessage("command not found");
+        }
+      });
+    }
+  }
   return (
-    <div className={styles.inputWrapper}>
-      <p
-        style={{
-          color: "var(--green)",
-          fontWeight: "bold",
-          marginRight: "10px",
-        }}
-      >
-        {" "}
-        {"->"}{" "}
-      </p>
-      <p style={{ color: "var(--teal2)", fontWeight: "bold" }}> terminal</p>
-      <input
-        className={styles.input}
-        spellCheck={false}
-        autoFocus={true}
-        onBlur={({ target }) => target.focus()}
-      />
+    <div className={styles.mainTerminal}>
+      <div className={styles.inputWrapper}>
+        <p
+          style={{
+            color: isCommandValid ? "var(--green)" : "var(--red)",
+            fontWeight: "bold",
+            marginRight: "10px",
+          }}
+        >
+          {" "}
+          {"->"}{" "}
+        </p>
+        <p style={{ color: "var(--teal2)", fontWeight: "bold" }}> terminal</p>
+        <input
+          value={command}
+          onChange={(e) => setCommand(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
+          className={styles.input}
+          spellCheck={false}
+          autoFocus={true}
+          onBlur={({ target }) => target.focus()}
+        />
+      </div>
+      {message ? (
+        <div
+          style={{
+            color: isCommandValid ? "white" : "var(--red)",
+          }}
+        >
+          {message}
+        </div>
+      ) : null}
     </div>
   );
 };
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export type Command = {
+  command: string;
+  message: string;
+};
+
+export const Commands: Command[] = [{ command: "commands", message: "1\n2\n" }];
