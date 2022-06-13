75e84795ff1235b254e38e0382e936628159d9eb
qazalin <ghazal.nzhrzr@gmail.com>
2022-06-13 19:50:56 +0430

    feat(components): &apos;

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
     return (
         <div className={styles.about}>
             <p>
-                I'm a passionate programmer with a love for learning and
+                I&apos;m a passionate programmer with a love for learning and
                 growing. I thrive in fast-paced and innovative cultures. I see
                 tech as the magic to solve problems and build products that
                 inspire.
